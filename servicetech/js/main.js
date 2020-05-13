f135e12c6d7bc33089619928fbc16fcf25fddef6
Z1chi <55581497+Z1chi@users.noreply.github.com>
2020-05-13 21:43:11 +0300

    Commit1

---
+++ servicetech/js/main.js
@@ -0,0 +1,73 @@
+$(document).ready(function(){
+    $('.owl-carouse').owlCarousel({
+        loop:false,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        },
+        navText: ["<svg width=\"13\" height=\"14\" viewBox=\"0 0 13 14\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
+        "<path d=\"M6 1L12 7M12 7L6 13M12 7H0\" stroke=\"#333333\" stroke-width=\"2\" stroke-linejoin=\"round\"/>\n" +
+        "</svg>\n","<svg width=\"13\" height=\"14\" viewBox=\"0 0 13 14\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
+    "<path d=\"M6 1L12 7M12 7L6 13M12 7H0\" stroke=\"#333333\" stroke-width=\"2\" stroke-linejoin=\"round\"/>\n" +
+    "</svg>\n"]
+    })
+});
+$(document).ready(function(){
+    $('.owl-carousel-1').owlCarousel({
+        loop:false,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        },
+        navText: ["<svg width=\"13\" height=\"14\" viewBox=\"0 0 13 14\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
+        "<path d=\"M6 1L12 7M12 7L6 13M12 7H0\" stroke=\"#333333\" stroke-width=\"2\" stroke-linejoin=\"round\"/>\n" +
+        "</svg>\n","<svg width=\"13\" height=\"14\" viewBox=\"0 0 13 14\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
+        "<path d=\"M6 1L12 7M12 7L6 13M12 7H0\" stroke=\"#333333\" stroke-width=\"2\" stroke-linejoin=\"round\"/>\n" +
+        "</svg>\n"]
+    })
+});
+$(document).ready(function(){
+    $('.owl-carousel-2').owlCarousel({
+        loop:false,
+        margin:2,
+        nav:true,
+        autoWidth:true,
+        autoHeight: false,
+
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+
+        },
+        navText: ["<svg width=\"13\" height=\"14\" viewBox=\"0 0 13 14\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
+        "<path d=\"M6 1L12 7M12 7L6 13M12 7H0\" stroke=\"#333333\" stroke-width=\"2\" stroke-linejoin=\"round\"/>\n" +
+        "</svg>\n","<svg width=\"13\" height=\"14\" viewBox=\"0 0 13 14\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
+        "<path d=\"M6 1L12 7M12 7L6 13M12 7H0\" stroke=\"#333333\" stroke-width=\"2\" stroke-linejoin=\"round\"/>\n" +
+        "</svg>\n"]
+    })
+});
